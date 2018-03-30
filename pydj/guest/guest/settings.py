a9501b096131d02c5a248404dfda907c052fce7c
wangsumei1992 <31680061+wangsumei1992@users.noreply.github.com>
2018-03-30 20:07:22 +0800

    submit

--- pydj/guest/guest/settings.py
+++ pydj/guest/guest/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '12345678',
         'OPTIONS': {
-           'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
 },
     }
 }
