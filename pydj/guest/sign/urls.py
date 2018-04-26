8015b1804a7a9295c1277394e135cee4239407b6
Sophy <3120380093@qq.com>
2018-04-26 17:16:20 +0800

    add_event_sign

--- pydj/guest/sign/urls.py
+++ pydj/guest/sign/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
     url(r'^user_sign/', views_if.user_sign, name='user_sign'),
     #  ex : /api/sec_get_event_list/
     url(r'^sec_get_event_list/', views_if_sec.get_event_list, name='get_event_list'),
+    #  ex : /api/sec_add_event/
+    url(r'^sec_add_event/', views_if_sec.add_event, name='add_event'),
 ]
--- pydj/guest/sign/views_if_sec.py
+++ pydj/guest/sign/views_if_sec.py
@@ -1,11 +1,11 @@
 #coding=utf-8
+import requests
 from django.contrib import auth as django_auth
-import hashlib, base64
+import hashlib, base64, time
 from django.http import JsonResponse
 from sign.models import Event, Guest
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
-
 
 #用户认证
 def user_auth(request):
@@ -72,3 +72,71 @@ def get_event_list(request):
             return JsonResponse({'status':200,'message':'success','data':datas})
         else:
             return JsonResponse({'status':'10022','message':'query result is empty'})
+
+#用户签名+时间戳
+def user_sign(request):
+
+    client_time = request.POST.get('time', '')  #客户端时间戳
+    client_sign = request.POST.get('sign', '')  #客户端签名
+    if client_time == '' or client_sign == '':
+        return "sign null"
+
+    #服务器时间
+    now_time = time.time()
+    server_time = str(now_time).split('.')[0]
+    #获取时间差
+    time_difference = int(server_time) - int(client_time)
+    if time_difference >= 60 :
+        return "timeout"
+
+    #签名检查
+    md5 = hashlib.md5()
+    sign_str = client_time + "&Guest-Bugmaster"
+    sign_bytes_utf8 = sign_str.encode(encoding="utf-8")
+    md5.update(sign_bytes_utf8)
+    server_sign = md5.hexdigest()
+    print(server_sign)
+    if server_sign != client_sign:
+        return "sign error"
+    else:
+        return "sign right"
+#添加发布会接口----增加签名+时间戳
+def add_event(request):
+    sign_result = user_sign(request)
+    if sign_result == "error":
+        return JsonResponse({'status':1011,'message':'request error'})
+    elif sign_result == "sign null":
+        return JsonResponse({'status':1012,'message':'user sign null'})
+    elif sign_result == "timeout":
+        return JsonResponse({'status':1013,'message':'user sign timeout'})
+    elif sign_result == "sign fail":
+        return JsonResponse({'status':1014,'message':'user sign error'})
+
+    eid = request.POST.get('eid', '') #发布会id
+    name = request.POST.get('name', '') #发布会标题
+    limit = request.POST.get('limit', '') #发布会人数
+    status = request.POST.get('status', '') #状态
+    address = request.POST.get('address', '') #地址
+    start_time = request.POST.get('start_time', '') #发布会时间
+
+    if eid == '' or name == '' or limit == '' or address == '' or start_time == '':
+        return JsonResponse({'status':10021,'message':'parameter error'})
+
+    result = Event.objects.filter(id=eid)
+    if result:
+        return JsonResponse({'status':1022,'message':'event id already exists'})
+
+    result = Event.objects.filter(name = name)
+    if result:
+        return JsonResponse({'status':10023,'message':'event name already exists'})
+
+    if status == '':
+        status = 1
+    try:
+        Event.objects.create(id=eid, name=name, limit=limit, address=address,
+                             status=int(status), start_time=start_time)
+    except ValidationError as e:
+        error = 'start_time format error. It must be in YYYY-MM-DD HH:MM:SS format.'
+        return JsonResponse({'status':10024,'message':error})
+
+    return JsonResponse({'status':200 ,'message':'add event success'})
